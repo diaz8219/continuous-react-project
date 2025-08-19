68456810cce6f4ff49388c8f78d5394817b1f193
diaz8219 <delta.82198@gmail.com>
2025-08-19 07:00:29 -0500

    Add initial files

---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <div>Home</div>
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
