a2ef518384d2b0b6eba73f92035b2346404db6ba
diaz8219 <delta.82198@gmail.com>
2025-08-19 08:56:18 -0500

    feat: Add routing functionality

--- src/routes/routerApp.jsx
+++ src/routes/routerApp.jsx
@@ -1,8 +1,27 @@
-export const router = [
-    {
-        
-    },
-    {
-        
-    }
+import Login from "../views/Login";
+import Home from "../views/Home";
+import Services from "../views/Services";
+import Contact from "../views/Contact";
+
+export let routerApp = [
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+  {
+    path: "/services",
+    element: <Services />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  },
+  {
+    path: "*",
+    element: <h1>Error 404: Not found.</h1>
+  }
 ];
