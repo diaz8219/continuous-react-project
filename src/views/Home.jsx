5660df8629faa487b8d755e2caea527a64f91183
diaz8219 <delta.82198@gmail.com>
2025-08-19 08:15:34 -0500

    refactor: Remove deprecated React imports

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,6 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-
 const Home = () => {
   return (
-    <div>
-        <Header />
-        <div>Home</div>
-        <Footer />
-    </div>
+    <div>Home</div>
   )
 }
 
