b9cea65911865584555cc33af7e07aec7ad8f18a
diaz8219 <delta.82198@gmail.com>
2025-09-02 06:40:58 -0500

    feat: Add basic styling for Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,43 @@
+import './Login.css'
+import { useState } from "react";
+
 const LogIn = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className='form-container'>
+      <form className="form">
+        <p className="form-title">Sign in to your account</p>
+        <div className="input-container">
+          <input 
+            required=''
+            autoComplete='off'
+            type="email" 
+            placeholder="Enter email"
+            id='email'
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <span></span>
+        </div>
+        <div className="input-container">
+          <input
+            required=''
+            autoComplete='off'
+            type="text" 
+            placeholder="Enter password"
+            id='email'
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </div>
+        <button type="submit" onClick={logIn} className="submit">
+          Sign in
+        </button>
 
-export default LogIn
+        <p className="signup-link">
+          No account?
+          <a href="">Sign up</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LogIn;
